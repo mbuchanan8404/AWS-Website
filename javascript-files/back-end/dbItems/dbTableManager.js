eed6591e2dcabcbe3310fe64a3a47ad103f951ec
Matthew Buchanan <matthew.buchanan@smoothstack.com>
2022-12-03 16:33:12 -0600

    Commit

--- javascript-files/back-end/dbItems/dbTableManager.js
+++ javascript-files/back-end/dbItems/dbTableManager.js
@@ -22,8 +22,8 @@ const AWS = require('aws-sdk');
 /* Update the Amazon Web Services DynamoDB access control credentials */
 AWS.config.update({
     region: "us-east-1",
-    accessKeyId: "AKIATHORXF4C6Y2PW47I",
-    secretAccessKey: "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"
+    accessKeyId: "",
+    secretAccessKey: ""
 });
 
 
